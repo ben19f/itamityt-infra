8a90692b138cbf46e7f91f64604ff545d02811c8
ben <2390188@mail.ru>
2026-03-13 19:44:25 +0300

    api adress

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -2,7 +2,7 @@ const form = document.getElementById("registerForm");
 const messageEl = document.getElementById("message");
 
 // URL бэкенда через Nginx proxy на /api
-const API_URL = "https://itamityt.ru/api";  
+const API_URL = "/api";  
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
