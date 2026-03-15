46ff76c3da02dc9fa47c941b54d55590a7a279a4
ben <2390188@mail.ru>
2026-03-15 08:21:38 +0300

    исправил pub2

--- app/api/public.py
+++ app/api/public.py
@@ -1,35 +1,36 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.session import get_db
+from db.deps import get_db
 from db.crud_user import get_user_by_username, get_last_users
 from db.crud_item import get_items_by_user
-
-from schemas.item import Item
 
 router = APIRouter(prefix="/public", tags=["public"])
 
 
-@router.get("/profile/{username}", response_model=list[Item])
-def public_profile(username: str, db: Session = Depends(get_db)):
-    user = get_user_by_username(db, username)
+@router.get("/profile/{username}")
+async def public_profile(username: str, db: AsyncSession = Depends(get_db)):
+
+    user = await get_user_by_username(db, username)
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    items = get_items_by_user(db, user.id)
+    items = await get_items_by_user(db, user.id)
 
     return items
 
 
 @router.get("/last-users")
-def last_users(db: Session = Depends(get_db)):
-    users = get_last_users(db, limit=3)
+async def last_users(db: AsyncSession = Depends(get_db)):
+
+    users = await get_last_users(db, limit=3)
 
     result = []
 
     for user in users:
-        items = get_items_by_user(db, user.id)
+
+        items = await get_items_by_user(db, user.id)
 
         result.append({
             "username": user.username,
