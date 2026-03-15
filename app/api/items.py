2e7e7593e393fba419c2d1f87af67357e83dcf3d
ben <2390188@mail.ru>
2026-03-15 23:33:59 +0300

    исправил items

--- app/api/items.py
+++ app/api/items.py
@@ -1,36 +1,21 @@
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.deps import get_db
 from db.crud_item import create_item, get_all_items, get_items_by_user, delete_item
-
 from schemas.item import ItemCreate, Item
 from models.user import User
+from core.security import get_current_user
 
-from core.security import get_current_user  # если есть функция для current_user
-
-# router = APIRouter()
 router = APIRouter(prefix="/items", tags=["items"])
 
 
-
-@router.get("/", response_model=list[Item])
+@router.get("", response_model=list[Item])
 async def read_items(db: AsyncSession = Depends(get_db)):
     items = await get_all_items(db)
     return items
 
 
-
-# @router.post("/items/", response_model=Item)
-# async def add_item(item: ItemCreate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
-#     new_item = await create_item(
-#         db,
-#         item.name,
-#         item.description,
-#         owner_user_id=current_user.id  # <-- вот тут
-#     )
-#     return new_item
-@router.post("/items/", response_model=Item)
+@router.post("", response_model=Item)
 async def add_item(
     item: ItemCreate,
     db: AsyncSession = Depends(get_db),
@@ -52,6 +37,7 @@ async def add_item(
 async def read_user_items(user_id: int, db: AsyncSession = Depends(get_db)):
     items = await get_items_by_user(db, user_id)
     return items
+
 
 @router.delete("/{item_id}")
 async def remove_item(item_id: int, db: AsyncSession = Depends(get_db)):
