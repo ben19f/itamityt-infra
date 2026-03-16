2dd33fda9d1d3fa63b1de5a31d588cc31f0a0373
ben <2390188@mail.ru>
2026-03-16 05:00:11 +0300

    deps к удалению

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,15 +1,15 @@
-from fastapi import Depends, HTTPException
-from jose import jwt, JWTError
-from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordBearer
+# from fastapi import Depends, HTTPException
+# from jose import jwt, JWTError
+# from sqlalchemy.orm import Session
+# from fastapi.security import OAuth2PasswordBearer
+#
+# from db.session import get_db
+# from models.user import User
+# # from core.security import SECRET_KEY, ALGORITHM
+# from core.config import settings
 
-from db.session import get_db
-from models.user import User
-# from core.security import SECRET_KEY, ALGORITHM
-from core.config import settings
 
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 # def get_current_user(
@@ -34,16 +34,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 #     return user
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
-    try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
-        email: str = payload.get("sub")
-        if email is None:
-            raise HTTPException(status_code=401, detail="Invalid authentication")
-    except Exception:
-        raise HTTPException(status_code=401, detail="Invalid authentication")
-
-    user = await get_user_by_email(db, email)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+# async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
+#     try:
+#         payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+#         email: str = payload.get("sub")
+#         if email is None:
+#             raise HTTPException(status_code=401, detail="Invalid authentication")
+#     except Exception:
+#         raise HTTPException(status_code=401, detail="Invalid authentication")
+#
+#     user = await get_user_by_email(db, email)
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return user
