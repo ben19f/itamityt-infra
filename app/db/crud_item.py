212bcf46dc247773aa7d9303656e3b9fbb5e5d2f
ben <2390188@mail.ru>
2026-03-16 06:57:17 +0300

    адресс редиректа

--- app/db/crud_item.py
+++ app/db/crud_item.py
@@ -57,7 +57,7 @@ async def create_item(db: AsyncSession, name: str, description: str, owner_user_
     try:
         async with httpx.AsyncClient() as client:
             await client.post(
-                "http://redirect:8000/links",
+                "http://redirect:8200/links",
                 json={
                     "link_id": db_item.link_id,
                     "original_url": db_item.description,
