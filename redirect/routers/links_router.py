fd431928509e04a7d00f3cb0c6c52c9523a76e43
ben <2390188@mail.ru>
2026-03-16 20:24:53 +0300

    endpoint links

--- redirect/routers/links_router.py
+++ redirect/routers/links_router.py
@@ -1,17 +1,45 @@
 from fastapi import APIRouter, HTTPException, Request, Depends
+from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from crud import get_link, log_click
-from database import get_db  # теперь импорт из database.py, без цикла
+from crud import get_link, log_click, create_link
+from database import get_db
+from schemas import LinkCreate
 
 router = APIRouter()
 
+
+@router.post("/links")
+async def create_link_endpoint(
+    data: LinkCreate,
+    db: AsyncSession = Depends(get_db)
+):
+    link = await create_link(
+        db,
+        data.link_id,
+        data.original_url,
+        data.owner_user_id
+    )
+
+    return link
+
+
 @router.get("/r/{link_id}")
-async def redirect_link(link_id: str, request: Request, db: AsyncSession = Depends(get_db)):
+async def redirect_link(
+    link_id: str,
+    request: Request,
+    db: AsyncSession = Depends(get_db)
+):
     link = await get_link(db, link_id)
+
     if not link:
         raise HTTPException(status_code=404, detail="Link not found")
 
-    await log_click(db, link_id, request.client.host, request.headers.get("user-agent"))
+    await log_click(
+        db,
+        link_id,
+        request.client.host,
+        request.headers.get("user-agent")
+    )
 
     return RedirectResponse(url=link.original_url)
